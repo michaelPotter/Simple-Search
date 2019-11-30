21d8c135ae179af433fba76fb1bdf7064375b425
michaelPotter <mpotter.dev@gmail.com>
2019-11-29 22:34:18 -0800

    add text menu search support

---
+++ app/src/main/java/de/tobiasbielefeld/searchbar/helper/TextSearch.java
@@ -0,0 +1,25 @@
+
+package de.tobiasbielefeld.searchbar.helper;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import de.tobiasbielefeld.searchbar.classes.CustomAppCompatActivity;
+import de.tobiasbielefeld.searchbar.helper.WebSearch;
+
+import static de.tobiasbielefeld.searchbar.SharedData.*;
+
+/**
+ * This class is to implement searching from the text highlight menu.
+ */
+public class TextSearch extends CustomAppCompatActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		CharSequence text = getIntent()
+			.getCharSequenceExtra(Intent.EXTRA_PROCESS_TEXT);
+		WebSearch.webSearch(text.toString().trim(), this);
+	}
+
+}
